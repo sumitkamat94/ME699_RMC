2d5cf1e877300c5c68526c4fe79924140a1ecff1
Landon Clark <landonclark97@gmail.com>
2021-02-17 00:34:46 -0500

    work

--- julia/odes/startup.jl
+++ julia/odes/startup.jl
@@ -39,7 +39,8 @@ end
 function do_ik!(state::MechanismState,
                 desired::Point3D,
                 path,
-                body;
+                body,
+                constraint;
                 a=0.01)
 
     lambda = 0.5
@@ -48,7 +49,8 @@ function do_ik!(state::MechanismState,
     mechanism = state.mechanism;
     world = root_frame(mechanism);
 
-    Jp = point_jacobian(state, path, transform(state, point, world))
+    point_in_world = get_eef_loc(state,body)
+    Jp = point_jacobian(state, path, transform(state, point_in_world, world))
 
     q = copy(configuration(state));
     
@@ -56,12 +58,23 @@ function do_ik!(state::MechanismState,
 
     while norm(dq)>0.0001
         point_in_world = get_eef_loc(state,body)
-        println(point_in_world)
 
         Jp = point_jacobian!(Jp, state, path, point_in_world)
         J = Array(Jp)
+
         # remove orientation part of Jacobian
         J = J[1:size(J, 1) .<=3 , :]
+
+        # zero out the constrained joints
+        if constraint
+            J[1] = 0.0
+            J[2] = 0.0
+            J[3] = 0.0
+            J[4] = 0.0
+            J[5] = 0.0
+            J[6] = 0.0
+        end
+
         # Compute an update in joint coordinates using the jacobian transpose
         dq = a * (inv((J'*J) + (square(lambda)*Matrix(1.0I,dof,dof)))*J' *(transform(state,desired,world) - point_in_world).v)
         #println(dq)
@@ -95,9 +108,40 @@ base_body = b[1]
 left_eef_path = path(mechanism,base_body,left_eef_body)
 right_eef_path = path(mechanism,base_body,right_eef_body)
 
-point = Point3D(root_frame(mechanism),-2,1,0.5)
+constrained = false
 
-do_ik!(state,point,left_eef_path,left_eef_body)
+while true
 
+    global constrained
+   
+    println("input desired x:")
+    x = parse(Float64,readline(stdin))
+    println("input desired y:")
+    y = parse(Float64,readline(stdin))
+    println("input desired z:")
+    z = parse(Float64,readline(stdin))
 
-transform(state, point, root_frame(mechanism))
+    goal_point = Point3D(root_frame(mechanism),x,y,z)
+
+    eef_l_loc = get_eef_loc(state,left_eef_body)
+    eef_r_loc = get_eef_loc(state,right_eef_body)
+
+    if norm((goal_point-eef_l_loc).v) < norm((goal_point-eef_r_loc).v)
+        do_ik!(state,goal_point,left_eef_path,left_eef_body,constrained)
+        constrained = true
+    else
+        do_ik!(state,goal_point,right_eef_path,right_eef_body,constrained)
+        constrained = true
+    end
+
+    println("continue?[y/n]")
+    res = readline(stdin)
+
+    if res == "y"
+        continue
+    elseif res == "n"
+        break
+    else
+        println("unrecognized response, continuing")
+    end
+end
