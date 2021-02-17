aaa17443789fd729240a543fc75e7e77518344d0
Landon Clark <landonclark97@gmail.com>
2021-02-16 23:13:27 -0500

    work

--- julia/odes/startup.jl
+++ julia/odes/startup.jl
@@ -38,8 +38,9 @@ end
 
 function do_ik!(state::MechanismState,
                 desired::Point3D,
-                path;
-                a=0.1)
+                path,
+                body;
+                a=0.01)
 
     lambda = 0.5
     dof = 10
@@ -47,19 +48,23 @@ function do_ik!(state::MechanismState,
     mechanism = state.mechanism;
     world = root_frame(mechanism);
 
+    Jp = point_jacobian(state, path, transform(state, point, world))
+
     q = copy(configuration(state));
     
     dq=10;
 
     while norm(dq)>0.0001
-        J = Array(geometric_jacobian(state, p))
+        point_in_world = get_eef_loc(state,body)
+        println(point_in_world)
+
+        Jp = point_jacobian!(Jp, state, path, point_in_world)
+        J = Array(Jp)
         # remove orientation part of Jacobian
         J = J[1:size(J, 1) .<=3 , :]
-
         # Compute an update in joint coordinates using the jacobian transpose
-        dq = a * (inv(J'*J + square(lambda)*Matrix(1.0I,dof,dof))*J' *(transform(state, desired, world) - desired).v)
-        println(dq)
-
+        dq = a * (inv((J'*J) + (square(lambda)*Matrix(1.0I,dof,dof)))*J' *(transform(state,desired,world) - point_in_world).v)
+        #println(dq)
         # Apply the update
         q .= configuration(state) .+ dq
 
@@ -71,8 +76,10 @@ function do_ik!(state::MechanismState,
 end
 
 
-
- 
+function get_eef_loc(state, body)
+    t = transform_to_root(state,body)
+    return Point3D(root_frame(state.mechanism),t.mat[13],t.mat[14],t.mat[15])
+end
  
 
 mvis, mechanism = display_urdf("10dofRobot.urdf",vis)
@@ -89,7 +96,8 @@ left_eef_path = path(mechanism,base_body,left_eef_body)
 right_eef_path = path(mechanism,base_body,right_eef_body)
 
 point = Point3D(root_frame(mechanism),-2,1,0.5)
-do_ik!(state,point,left_eef_path)
+
+do_ik!(state,point,left_eef_path,left_eef_body)
 
 
 transform(state, point, root_frame(mechanism))
